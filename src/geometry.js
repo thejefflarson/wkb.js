c7175c416553c1097fa7ea02b6a0cc5ab31af225
Jeff Larson <thejefflarson@gmail.com>
2011-11-10 18:31:51 -0500

    move templates to static places. I'm not sure about it

--- src/geometry.js
+++ src/geometry.js
@@ -21,7 +21,7 @@ wkb.Utils.mixin(wkb.Geometry, {
   registerParser : function(type, fn){
     this["parse" + type] = function(data) {
       var instance = new (this.prototype.constructor)(data);
-      var mixin = fn(instance);
+      var mixin = fn();
       wkb.Utils.mixin(instance, mixin);
       if(instance.parse) instance.parse();
       return instance;
@@ -30,40 +30,54 @@ wkb.Utils.mixin(wkb.Geometry, {
 });
 
 // templates
-wkb.Geometry.registerParser("WKB", function(instance){
-  return {
-    endian : function(){
-      return !!this.data.getUint8(0);
-    },
+wkb.Geometry.WKBTemplate = {
+  endian : function(){
+    return !!this.data.getUint8(0);
+  },
 
-    numGeometries : function(){
-                                 // type + numrings
-      return this.data.getUint32(wkb.Type.b.Int8 + wkb.Type.b.Uint32, this.endian());
-    },
+  numGeometries : function(){
+                               // type + numrings
+    return this.data.getUint32(wkb.Type.b.Int8 + wkb.Type.b.Uint32, this.endian());
+  },
 
-    byteOffset : function(){
-      return wkb.Type.b.Int8 + wkb.Type.b.Uint32 * 2;
-    },
+  byteOffset : function(){
+    return wkb.Type.b.Int8 + wkb.Type.b.Uint32 * 2;
+  },
 
-    byteLength : function(){
-      return this.byteOffset() + this.geometries.reduce(function(memo, ring){ return ring.byteLength() + memo; }, 0);
-    },
+  byteLength : function(){
+    return this.byteOffset() + this.geometries.reduce(function(memo, ring){ return ring.byteLength() + memo; }, 0);
+  },
 
-    parse : function(){
-      wkb.Utils.assert(this.data.getUint32(1, this.endian()) !== wkb.Type.k.wkbUnknown, "Geometry is an abstract type");
-      wkb.Utils.assert(this.data.getUint32(1, this.endian()) === this.type, "Wrong type for " + this);
-      var offset = this.byteOffset();
-      for(var i = 0; i < this.numGeometries(); i++){
-        var child = this._child.parseWKB(new DataView(this.data.buffer, this.data.byteOffset + offset));
-        child.endian = this.endian();
-        this.geometries.push(child);
-        offset = child.byteLength() + offset;
-      }
+  _peekChild : function (){
+    return this._child;
+  },
+
+  parse : function(){
+    wkb.Utils.assert(this.data.getUint32(1, this.endian()) !== wkb.Type.k.wkbUnknown, "Geometry is an abstract type");
+    wkb.Utils.assert(this.data.getUint32(1, this.endian()) === this.type, "Wrong type for " + this);
+    var offset = this.byteOffset();
+    for(var i = 0; i < this.numGeometries(); i++){
+      var child = this._peekChild(offset).parseWKB(new DataView(this.data.buffer, this.data.byteOffset + offset));
+      child.endian = this.endian();
+      this.geometries.push(child);
+      offset = child.byteLength() + offset;
     }
-  };
+  }
+};
+
+wkb.Geometry.WKTTemplate = {
+  parse : function(){
+
+  }
+}
+
+wkb.Geometry.registerParser("WKB", function(instance){
+  return wkb.Geometry.WKBTemplate;
 });
 
-wkb.Geometry.registerParser("WKT", function(text){});
+wkb.Geometry.registerParser("WKT", function(text){
+  return wkb.Geometry.WKTTemplate;
+});
 
 wkb.Geometry.registerParser("JSON", function(json){});
 
